7583ca5aa9e45b4b4ca93d7a8290eb6829d58e25
Maicon Boer <maicon_boer@hotmail.com>
2020-07-12 17:20:08 -0300

    add Access-Control-Allow-Origin

--- src/server.js
+++ src/server.js
@@ -6,6 +6,13 @@ const methodOverride = require('method-override')
 
 const server = express()
 server.use(cors())
+server.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 
 server.use(express.urlencoded({ extended: true }))
 
