683c413dd3268a7b604c0953be52ba00a58b296b
Maicon Boer <maicon_boer@hotmail.com>
2020-07-13 13:29:20 -0300

    status code and review adjustments

--- src/controllers/starredRepositories.js
+++ src/controllers/starredRepositories.js
@@ -4,12 +4,12 @@ const StarredRepositories = require('../models/StarredRepositories')
 module.exports = {
   async index(req, res) {   
     try {
-      const results = await StarredRepositories.all()
-      const respositories = results.rows
-    
-      return res.json(respositories)
+      const results = await StarredRepositories.all();
+      const respositories = results.rows;
+      return res.status(200).json(respositories);
+
     } catch (error) {
-      console.log(error);
+      return res.status(404).json({ message: error });
     }
 
     
@@ -17,59 +17,59 @@ module.exports = {
 
   async findby(req, res) {
     try {
-      const { filter, id } = req.params
-      const results = await StarredRepositories.findBy(filter, id)
-      const respositories = results.rows
-    
-      return res.json(respositories)
+      const { filter, id } = req.params;
+      const results = await StarredRepositories.findBy(filter, id);
+      const respositories = results.rows;
+      return res.status(200).json(respositories);
+
     } catch (error) {
-      console.log(error);
+      return res.status(404).json({ message: error });
     }
     
   },
 
   async post(req, res) {
     try {
-      const results = await StarredRepositories.create(req.body)
-      const repository = results.rows[0]
-      return res.json(repository)
+      const results = await StarredRepositories.create(req.body);
+      const repository = results.rows[0];
+      return res.status(201).json(repository);
 
     } catch (error) {
-      console.log(error);
+      return res.status(400).send({ message: 'this tag already exists!'});
     }
   },
 
   async show(req, res) {   
     try {
-      const results = await StarredRepositories.find(req.params.id)
-      const repositories = results.rows
-
-      return res.json( repositories )
+      const results = await StarredRepositories.find(req.params.id);
+      const repositories = results.rows;
+      return res.status(200).json(repositories);
     } catch (error) {
+
       console.log(error);
+      return res.status(404).json({ message: error});
     }
   },
 
   async put(req, res) {
     try {
-      const results = await StarredRepositories.update(req.body)
-      const repository = results.rows[0]
-      return res.json(repository)        
+      const results = await StarredRepositories.update(req.body);
+      const repository = results.rows[0];
+      return res.status(200).json(repository);       
       
     } catch (error) {
-      console.log(error);
+      return res.status(400).json({ message: 'this tag already exists!'});
     }     
   },
 
   async delete(req, res) {
     try {     
-      const results = await StarredRepositories.delete(req.params.id)
+      const results = await StarredRepositories.delete(req.params.id);
       if(results.rowCount > 0) {
-        return res.json({ message: 'Deleted with success!' })
+        return res.status(200).json({ message: 'Deleted with success!' });
       }
-        
     } catch (error) {
-      console.log(error);
+      return res.status(404).json({ message: 'tag not found!'});
     }
   },
 }
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,12 @@
-const express = require('express')
-const routes = express.Router()
-const starredRepositories = require('./controllers/starredRepositories')
+const express = require('express');
+const routes = express.Router();
+const starredRepositories = require('./controllers/starredRepositories');
 
-routes.get('/test', (req, res) => {
-  return res.json({ message: 'Api funcionando'})
-} )
-routes.get('/starred-repositories', starredRepositories.index)
-routes.get('/starred-repositories/:filter/:id', starredRepositories.findby)
-routes.post('/starred-repositories', starredRepositories.post)
-routes.get('/starred-repositories/:id', starredRepositories.show)
-routes.put('/starred-repositories', starredRepositories.put)
-routes.delete('/starred-repositories/:id', starredRepositories.delete)
+routes.get('/starred-repositories', starredRepositories.index);
+routes.get('/starred-repositories/:filter/:id', starredRepositories.findby);
+routes.post('/starred-repositories', starredRepositories.post);
+routes.get('/starred-repositories/:id', starredRepositories.show);
+routes.put('/starred-repositories', starredRepositories.put);
+routes.delete('/starred-repositories/:id', starredRepositories.delete);
 
-module.exports = routes
+module.exports = routes;
